bfb9aa6c077825b5a4083bd5255efc5ebbb379ad
alnym <alnym@ALNYM>
2024-10-16 22:00:54 +0300

    Add project files.

---
+++ TravelInspiration.API/Features/SearchDestinations/SearchDestinations.cs
@@ -0,0 +1,23 @@
+﻿using TravelInspiration.API.Shared.Networking;
+
+namespace TravelInspiration.API.Features.SearchDestinations;
+
+public static  class SearchDestinations
+{
+    public static void AddEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/destinations",
+            async (string? searchFor,
+                ILoggerFactory logger,
+                IDestinationSearchApiClient destinationSearchApiClient) =>
+            {
+                logger.CreateLogger("EndpointsHandlers")
+                    .LogInformation("SearchDestinations feature called.");
+
+                var result = await destinationSearchApiClient
+                    .GetDestinationsAsync(searchFor, null);
+                
+                return Results.Ok(result);
+            });
+    }
+}
